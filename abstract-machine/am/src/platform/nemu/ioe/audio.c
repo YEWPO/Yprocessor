cb630997e460e3b1aec70e44a74b8361d5097417
22050908-Meng Pei <2398696309@qq.com>
2024-02-27 22:00:26 +0800

    audio

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,19 +8,43 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+static uint32_t buffer_size;
+static int wd = 0;
+
+static void audio_write(uint8_t *buf, int len) {
+  uint8_t *audio_sbuf = (uint8_t *)AUDIO_SBUF_ADDR;
+
+  while (inl(AUDIO_COUNT_ADDR) + len > buffer_size);
+  int nwrite = 0;
+  while (nwrite < len) {
+    outb((uintptr_t)(audio_sbuf + wd), buf[nwrite]);
+    nwrite++;
+    wd = (wd + 1) % buffer_size;
+  }
+  outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len);
+}
+
 void __am_audio_init() {
+  buffer_size = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = buffer_size;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int len = ctl->buf.end - ctl->buf.start;
+  audio_write(ctl->buf.start, len);
 }
