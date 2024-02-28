200ca1574a02f6e918d6ebab19ba25481b0c8db1
22050908-Meng Pei <2398696309@qq.com>
2024-02-28 16:20:12 +0800

    cte添加事件识别

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,6 +8,10 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 0xb:
+        c->mepc += 4;
+        ev.event = c->GPR1 == -1 ? EVENT_YIELD : EVENT_SYSCALL;
+        break;
       default: ev.event = EVENT_ERROR; break;
     }
 
