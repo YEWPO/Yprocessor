261ab99d74938041fefb15bc9cdebea771fc1668
22050908-Meng Pei <2398696309@qq.com>
2024-02-28 18:29:40 +0800

    strace

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,9 +1,35 @@
 #include <common.h>
 #include "syscall.h"
 
+const char *syscall_name[] = {
+  "exit",
+  "yield",
+  "open",
+  "read",
+  "write",
+  "kill",
+  "getpid",
+  "close",
+  "lseek",
+  "brk",
+  "fstat",
+  "time",
+  "signal",
+  "execve",
+  "fork",
+  "link",
+  "unlink",
+  "wait",
+  "times",
+  "gettimeofday",
+};
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
     case SYS_exit:
@@ -14,4 +40,8 @@ void do_syscall(Context *c) {
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+#ifdef STRACE
+  printf("%s (%ld, %ld, %ld) = %ld\n", syscall_name[a[0]], a[1], a[2], a[3], c->GPRx);
+#endif
 }
