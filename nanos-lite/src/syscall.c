3a1b2060d713131013703a55e14e3b0f70e0ae09
22050908-Meng Pei <2398696309@qq.com>
2024-02-28 18:14:59 +0800

    sys_yield系统调用

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,17 @@
 #include <common.h>
 #include "syscall.h"
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_exit:
+      halt(c->GPR2);
+      break;
+    case SYS_yield:
+      c->GPRx = 0;
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
