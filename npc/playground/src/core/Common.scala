274288c54039937eaf1a1de59f7066316d82170d
22050908-Meng Pei <2398696309@qq.com>
2024-03-11 20:40:42 +0800

    fix: change Commom.asUInt64 to use XLEN in CoreConfig

--- npc/playground/src/core/Common.scala
+++ npc/playground/src/core/Common.scala
@@ -3,18 +3,19 @@ package core
 import chisel3._
 import chisel3.util.Cat
 import chisel3.util.Fill
+import core.CoreConfig._
 
 object Common {
-  def asUInt64[T <: Bits](bits: T): UInt = {
+  def asUInt[T <: Bits](bits: T): UInt = {
     val len = bits.getWidth
-    val extendLen = 64 - len
+    val extendLen = XLEN - len
 
     Cat(Fill(extendLen, 0.U(1.W)), bits)
   }
 
-  def asSInt64[T <: Bits](bits: T): SInt = {
+  def asSInt[T <: Bits](bits: T): SInt = {
     val len = bits.getWidth
-    val extendLen = 64 - len
+    val extendLen = XLEN - len
 
     Cat(Fill(extendLen, bits(len - 1)), bits).asSInt
   }
--- npc/playground/src/core/modules/ImmGen.scala
+++ npc/playground/src/core/modules/ImmGen.scala
@@ -19,18 +19,18 @@ object InstType {
 
 class ImmGen(xlen: Int) extends Module {
   val io = IO(new Bundle {
-    val inst = Input(UInt(xlen.W))
-    val instType = Input(UInt(typeWidth.W))
+    val inst      = Input(UInt(xlen.W))
+    val instType  = Input(UInt(typeWidth.W))
 
-    val imm = Output(UInt(xlen.W))
+    val imm       = Output(UInt(xlen.W))
   })
 
-  val immR = asSInt64(0.U)
-  val immI = asSInt64(io.inst(31, 20))
-  val immS = asSInt64(Cat(io.inst(31, 25), io.inst(11, 7)))
-  val immB = asSInt64(Cat(io.inst(31), io.inst(7), io.inst(30, 25), io.inst(11, 8), 0.U(1.W)))
-  val immU = asSInt64(Cat(io.inst(31, 12), 0.U(12.W)))
-  val immJ = asSInt64(Cat(io.inst(31), io.inst(19, 12), io.inst(20), io.inst(30, 21), 0.U(1.W)))
+  val immR = asSInt(0.U)
+  val immI = asSInt(io.inst(31, 20))
+  val immS = asSInt(Cat(io.inst(31, 25), io.inst(11, 7)))
+  val immB = asSInt(Cat(io.inst(31), io.inst(7), io.inst(30, 25), io.inst(11, 8), 0.U(1.W)))
+  val immU = asSInt(Cat(io.inst(31, 12), 0.U(12.W)))
+  val immJ = asSInt(Cat(io.inst(31), io.inst(19, 12), io.inst(20), io.inst(30, 21), 0.U(1.W)))
 
   io.imm := MuxLookup(io.instType, 0.U(xlen.W))(Seq(
     R -> immR.asUInt,
