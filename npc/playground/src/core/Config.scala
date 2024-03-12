eefeaf8e6978b037bbd8213cbec3eaff04495e25
22050908-Meng Pei <2398696309@qq.com>
2024-03-12 18:50:40 +0800

    feat: add gprs

--- npc/playground/src/core/Config.scala
+++ npc/playground/src/core/Config.scala
@@ -1,5 +1,9 @@
 package core
 
 object CoreConfig {
-  val XLEN = 64
+  val XLEN          = 64
+
+  val GPR_NUM       = 32
+  val GPR_LEN       = 5
+  val CSR_LEN       = 12
 }
--- npc/playground/src/core/modules/Gpr.scala
+++ npc/playground/src/core/modules/Gpr.scala
@@ -1,6 +1,52 @@
 package core.modules
 
 import chisel3._
+import core.CoreConfig._
+
+class GprForward extends Module {
+  val io = IO(new Bundle {
+    val oriDatas    = Input(Vec(GPR_NUM, UInt(XLEN.W)))
+
+    val rdWbu       = Input(UInt(GPR_LEN.W))
+    val dataWbu     = Input(UInt(XLEN.W))
+    val rdLsu       = Input(UInt(GPR_LEN.W))
+    val dataLsu     = Input(UInt(XLEN.W))
+
+    val tarDatas    = Output(Vec(GPR_NUM, UInt(XLEN.W)))
+  })
+
+  io.tarDatas           := io.oriDatas
+  io.tarDatas(io.rdWbu) := io.dataWbu
+  io.tarDatas(io.rdLsu) := io.dataLsu
+}
 
 class Gpr extends Module {
+  val io = IO(new Bundle {
+    val rdWbu     = Input(UInt(GPR_LEN.W))
+    val dataWbu   = Input(UInt(XLEN.W))
+    val rdLsu     = Input(UInt(GPR_LEN.W))
+    val dataLsu   = Input(UInt(XLEN.W))
+    val rs1       = Input(UInt(GPR_LEN.W))
+    val rs2       = Input(UInt(GPR_LEN.W))
+
+    val src1      = Output(UInt(XLEN.W))
+    val src2      = Output(UInt(XLEN.W))
+  })
+
+  val gprs = RegInit(VecInit(Seq.fill(GPR_NUM)(0.U(XLEN.W))))
+  gprs(io.rdWbu) := io.dataWbu
+
+  val gprForward = Module(new GprForward)
+  gprForward.io.oriDatas  := gprs
+  gprForward.io.rdWbu     := io.rdWbu
+  gprForward.io.rdLsu     := io.rdLsu
+  gprForward.io.dataWbu   := io.dataWbu
+  gprForward.io.dataLsu   := io.dataLsu
+
+  val trueGpr = Wire(Vec(GPR_NUM, UInt(XLEN.W)))
+  trueGpr     := gprForward.io.tarDatas
+  trueGpr(0)  := 0.U(XLEN.W)
+
+  io.src1 := trueGpr(io.rs1)
+  io.src2 := trueGpr(io.rs2)
 }
