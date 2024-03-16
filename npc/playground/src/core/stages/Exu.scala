76f240084ab5b4f9b5e5a600d21fedb266562ed8
22050908-Meng Pei <2398696309@qq.com>
2024-03-16 17:51:00 +0800

    feat: add exsrc1 & exsrc2

--- npc/playground/src/core/stages/Exu.scala
+++ npc/playground/src/core/stages/Exu.scala
@@ -2,5 +2,15 @@ package core.stages
 
 import chisel3._
 
+object ExuSrc1 {
+  val SRC1  = false.B
+  val PC    = true.B
+}
+
+object ExuSrc2 {
+  val IMM   = false.B
+  val SRC2  = true.B
+}
+
 class Exu extends Module {
 }
