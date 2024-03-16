85dcc34437557cd9d7bad1fcd61b883ac1f8186b
22050908-Meng Pei <2398696309@qq.com>
2024-03-16 17:08:23 +0800

    feat: add PcGen

---
+++ npc/playground/src/core/modules/PcGen.scala
@@ -0,0 +1,36 @@
+package core.modules
+
+import chisel3._
+import core.CoreConfig._
+
+class SnpcGen extends Module {
+  val io = IO(new Bundle {
+    val pc        = Input(UInt(XLEN.W))
+    val instLen   = Input(UInt(2.W))
+
+    val snpc      = Output(UInt(XLEN.W))
+  })
+
+  io.snpc := io.pc + Mux(io.instLen.andR, 4.U, 2.U)
+}
+
+class PcGen extends Module {
+  val io = IO(new Bundle {
+    val pc        = Input(UInt(XLEN.W))
+    val instLen   = Input(UInt(2.W))
+    val control   = Input(Bool())
+    val dnpc      = Input(UInt(XLEN.W))
+
+    val snpc      = Output(UInt(XLEN.W))
+    val npc       = Output(UInt(XLEN.W))
+  })
+
+  val snpcGen = Module(new SnpcGen)
+  snpcGen.io.pc       := io.pc
+  snpcGen.io.instLen  := io.instLen
+
+  val snpc = snpcGen.io.snpc
+
+  io.snpc := snpc
+  io.npc  := Mux(io.control, io.dnpc, snpc)
+}
