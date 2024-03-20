5b3e4c46b4b0a519faa5d97bf799d852bd814507
22050908-Meng Pei <2398696309@qq.com>
2024-03-20 22:16:02 +0800

    feat: add axi4 write address bundle

--- npc/playground/src/bus/axi.scala
+++ npc/playground/src/bus/axi.scala
@@ -15,6 +15,40 @@ object ResponseType extends ChiselEnum {
   val OKAY, EXOKAY, SLVERR, DECERR = Value
 }
 
+class Axi4WriteAddrBundle extends Bundle {
+  val id        = UInt(1.W)
+  val addr      = UInt(XLEN.W)
+  val len       = UInt(8.W)
+  val size      = UInt(3.W)
+  val burst     = UInt(2.W)
+  val lock      = UInt(1.W)
+  val cache     = UInt(4.W)
+  val prot      = UInt(3.W)
+  val qos       = UInt(4.W)
+  val region    = UInt(4.W)
+}
+
+object Axi4WriteAddrBundle {
+  def apply(addr: UInt, len: UInt = 0.U, size: UInt): Axi4WriteAddrBundle = {
+    val aw = Wire(new Axi4WriteAddrBundle)
+
+    import BrustType._
+    import AccessType._
+    aw.id     := 0.U
+    aw.addr   := addr
+    aw.len    := len
+    aw.size   := size
+    aw.burst  := INCR
+    aw.lock   := NORMAL
+    aw.cache  := 0.U
+    aw.prot   := 0.U
+    aw.qos    := 0.U
+    aw.region := 0.U
+
+    aw
+  }
+}
+
 class Axi4ReadAddrBundle extends Bundle {
   val id        = UInt(1.W)
   val addr      = UInt(XLEN.W)
