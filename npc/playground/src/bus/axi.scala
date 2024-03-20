99e25d64146242c5490b2b0ec95677dc69030cce
22050908-Meng Pei <2398696309@qq.com>
2024-03-20 21:56:36 +0800

    feat: add apply method for axi4 read address

--- npc/playground/src/bus/axi.scala
+++ npc/playground/src/bus/axi.scala
@@ -3,15 +3,44 @@ package bus
 import chisel3._
 import core.CoreConfig._
 
-class AxiReadAddrBundle extends Bundle {
+object BrustType extends ChiselEnum {
+  val FIXED, INCR, WRAP = Value
+}
+
+object AccessType extends ChiselEnum {
+  val NORMAL, EXCLUSIVE = Value
+}
+
+class Axi4ReadAddrBundle extends Bundle {
   val id        = UInt(1.W)
   val addr      = UInt(XLEN.W)
   val len       = UInt(8.W)
   val size      = UInt(3.W)
   val burst     = UInt(2.W)
-  val lock      = UInt(2.W)
+  val lock      = UInt(1.W)
   val cache     = UInt(4.W)
   val prot      = UInt(3.W)
   val qos       = UInt(4.W)
   val region    = UInt(4.W)
+}
+
+object Axi4ReadAddrBundle {
+  def apply(addr: UInt, len: UInt = 0.U, size: UInt): Axi4ReadAddrBundle = {
+    val aw = Wire(new Axi4ReadAddrBundle)
+
+    import BrustType._
+    import AccessType._
+    aw.id     := 0.U
+    aw.addr   := addr
+    aw.len    := len
+    aw.size   := size
+    aw.burst  := INCR
+    aw.lock   := NORMAL
+    aw.cache  := 0.U
+    aw.prot   := 0.U
+    aw.qos    := 0.U
+    aw.region := 0.U
+
+    aw
+  }
 }
