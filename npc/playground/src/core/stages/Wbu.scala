2aceb0a20c582824cb6ed831b340b9d0631c6fe3
22050908-Meng Pei <2398696309@qq.com>
2024-03-26 16:01:51 +0800

    feat: add wbu

--- npc/playground/src/core/stages/Wbu.scala
+++ npc/playground/src/core/stages/Wbu.scala
@@ -1,6 +1,22 @@
 package core.stages
 
 import chisel3._
+import chisel3.util.Decoupled
+import core.CoreConfig._
 
 class Wbu extends Module {
+  val io = IO(new Bundle {
+    val wbuIn = Flipped(Decoupled(new Bundle {
+      val rd        = UInt(5.W)
+      val lsuData   = UInt(XLEN.W)
+    }))
+
+    val rd    = Output(UInt(5.W))
+    val data  = Output(UInt(XLEN.W))
+  })
+
+  io.wbuIn.ready := true.B
+
+  io.rd           := Mux(io.wbuIn.valid, io.wbuIn.bits.rd, 0.U)
+  io.data         := Mux(io.wbuIn.valid, io.wbuIn.bits.lsuData, 0.U)
 }
