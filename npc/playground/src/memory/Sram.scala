e13f9acc8831c577d0e2369811e35cafc62cbd19
22050908-Meng Pei <2398696309@qq.com>
2024-04-02 23:07:30 +0800

    feat: add sram write

--- npc/playground/src/memory/Sram.scala
+++ npc/playground/src/memory/Sram.scala
@@ -10,6 +10,10 @@ import chisel3.util.is
 import chisel3.util.Counter
 import chisel3.util.HasBlackBoxPath
 import chisel3.util.Cat
+import bus.Axi4WriteAddrBundle
+import bus.Axi4WriteDataBundle
+import bus.Axi4WriteRespBundle
+import core.CoreConfig._
 
 object SramReadState extends ChiselEnum {
   val rWait, rRead = Value
@@ -37,19 +41,19 @@ class SramRead extends Module {
   val arReg         = Reg(new Axi4ReadAddrBundle)
   arReg := Mux(ar.fire, ar.bits, arReg)
 
-  val (readCount, readDone) = Counter(r.fire, 2)
+  val (readCnt, readDone) = Counter(r.fire, 2)
 
   val sramRead = Module(new SramReadBlackBox)
   sramRead.io.clk := clock
   sramRead.io.en  := stateReg === rRead
-  sramRead.io.addr := arReg.addr + Cat(readCount, 0.U(3.W))
+  sramRead.io.addr := arReg.addr + Cat(readCnt, 0.U(3.W))
 
   ar.ready      := nextState === rWait
 
   r.valid       := stateReg === rRead
   r.bits        := Axi4ReadDataBundle(
     data = sramRead.io.data,
-    last = readCount === 1.U
+    last = readCnt === 1.U
   )
 
   nextState := rWait
@@ -64,11 +68,76 @@ class SramRead extends Module {
   }
 }
 
+object SramWriteState extends ChiselEnum {
+  val wWait, wWrite, wResp = Value
+}
+
+class SramWriteBlackBox extends BlackBox with HasBlackBoxPath {
+  val io = IO(new Bundle {
+    val clk     = Input(Clock())
+    val en      = Input(Bool())
+    val addr    = Input(UInt(XLEN.W))
+    val data    = Input(UInt(XLEN.W))
+    val strb    = Input(UInt(8.W))
+  })
+
+  addPath("src/memory/SramWrite.v")
+}
+
+class SramWrite extends Module {
+  val aw = Flipped(Decoupled(new Axi4WriteAddrBundle))
+  val w  = Flipped(Decoupled(new Axi4WriteDataBundle))
+  val b  = Decoupled(new Axi4WriteRespBundle)
+
+  import SramWriteState._
+  val stateReg      = RegInit(wWait)
+  val nextState     = Wire(SramWriteState())
+  stateReg := nextState
+
+  val awReg         = Reg(new Axi4WriteAddrBundle)
+  awReg := Mux(aw.fire, aw.bits, awReg)
+
+  val writeDone     = w.fire && w.bits.last
+  val writeCnt      = RegInit(0.U(8.W))
+  writeCnt := Mux(nextState === wWait, 0.U, Mux(w.fire, writeCnt + 1.U, writeCnt))
+
+  aw.ready         := nextState === wWait
+  w.ready          := stateReg === wWrite
+  b.valid          := stateReg === wResp
+  b.bits           := Axi4WriteRespBundle()
+
+  val sramWrite = Module(new SramWriteBlackBox)
+  sramWrite.io.clk    := clock
+  sramWrite.io.en     := stateReg === wWrite
+  sramWrite.io.addr   := awReg.addr + Cat(writeCnt, 0.U(3.W))
+  sramWrite.io.data   := w.bits.data
+  sramWrite.io.strb   := w.bits.strb
+
+  nextState := wWait
+  switch (stateReg) {
+    is (wWait) {
+      nextState := Mux(aw.fire, wWrite, wWait)
+    }
+
+    is (wWrite) {
+      nextState := Mux(writeDone, wResp, wWrite)
+    }
+
+    is (wResp) {
+      nextState := Mux(b.fire, wWait, wResp)
+    }
+  }
+}
+
 class Sram extends Module {
   val axi = new Axi4Bundle
 
-  val read = Module(new SramRead)
+  val read  = Module(new SramRead)
+  val write = Module(new SramWrite)
 
   read.ar     <> axi.ar
   read.r      <> axi.r
+  write.aw    <> axi.aw
+  write.w     <> axi.w
+  write.b     <> axi.b
 }
