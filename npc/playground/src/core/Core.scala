dd0e7842da02fa9a36fefee2de68a5248b19a33a
22050908-Meng Pei <2398696309@qq.com>
2024-04-02 17:56:56 +0800

    feat: Pipe Register

--- npc/playground/src/core/Core.scala
+++ npc/playground/src/core/Core.scala
@@ -1,6 +1,30 @@
 package core
 
 import chisel3._
+import chisel3.util.Decoupled
+
+class PipeReg[T <: Data](gen: T) extends Module {
+  val io = IO(new Bundle {
+    val in = Flipped(Decoupled(gen))
+    val out = Decoupled(gen)
+  })
+
+  val reg = Reg(gen)
+  val valid = RegInit(false.B)
+
+  io.in.ready := !valid
+  io.out.valid := valid
+  io.out.bits := reg
+
+  when(io.in.fire) {
+    reg := io.in.bits
+    valid := true.B
+  }
+
+  when(io.out.fire) {
+    valid := false.B
+  }
+}
 
 class Core extends Module {
 }
