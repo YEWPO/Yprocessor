fbc1abafc704e5d08e64bf81a405ab352dcf30fc
22050908-Meng Pei <2398696309@qq.com>
2024-04-02 18:13:34 +0800

    feat: core framework

--- npc/playground/src/core/Core.scala
+++ npc/playground/src/core/Core.scala
@@ -2,6 +2,14 @@ package core
 
 import chisel3._
 import chisel3.util.Decoupled
+import bus.Axi4Bundle
+import core.stages.Ifu
+import core.stages.Idu
+import core.stages.Exu
+import core.stages.Lsu
+import core.stages.Wbu
+import core.modules.Gpr
+import bus.AxiArbiter
 
 class PipeReg[T <: Data](gen: T) extends Module {
   val io = IO(new Bundle {
@@ -27,4 +35,29 @@ class PipeReg[T <: Data](gen: T) extends Module {
 }
 
 class Core extends Module {
+  val io = IO(new Bundle {
+    val axi = new Axi4Bundle
+  })
+
+  val ifu = Module(new Ifu)
+  val idu = Module(new Idu)
+  val exu = Module(new Exu)
+  val lsu = Module(new Lsu)
+  val wbu = Module(new Wbu)
+
+  val gpr = Module(new Gpr)
+
+  val axiArbiter = Module(new AxiArbiter)
+
+  val ifu2idu = Module(new PipeReg(ifu.io.ifuOut))
+  val idu2exu = Module(new PipeReg(idu.io.iduOut))
+  val exu2lsu = Module(new PipeReg(exu.io.exuOut))
+  val lsu2wbu = Module(new PipeReg(lsu.io.lsuOut))
+
+  idu.io.iduIn <> ifu2idu.io.out
+  exu.io.exuIn <> idu2exu.io.out
+  lsu.io.lsuIn <> exu2lsu.io.out
+  wbu.io.wbuIn <> lsu2wbu.io.out
+
+  io.axi <> axiArbiter.io.axi
 }
