9c7377b495b5ab5df0bcf9636ccd9eee558e41c5
22050908-Meng Pei <2398696309@qq.com>
2024-04-05 12:42:28 +0800

    feat: add npc init code

---
+++ nsim/src/isa/npc/init.c
@@ -0,0 +1,20 @@
+#include <isa.h>
+#include <memory/paddr.h>
+
+static const uint32_t img [] = {
+  0x00000297,  // auipc t0,0
+  0x00028823,  // sb  zero,16(t0)
+  0x0102c503,  // lbu a0,16(t0)
+  0x00100073,  // ebreak (used as nsim_trap)
+  0xdeadbeef,  // some data
+};
+
+static void restart() {
+  // TODO
+}
+
+void init_isa() {
+  memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
+
+  restart();
+}
