f0e240a3795f94d29863c15eb4dacda1bd9f91fd
22050908-Meng Pei <2398696309@qq.com>
2024-04-05 15:20:58 +0800

    feat: add dut.c

---
+++ nsim/src/isa/npc/difftest/dut.c
@@ -0,0 +1,20 @@
+#include <isa.h>
+#include <cpu/difftest.h>
+#include "../local-include/reg.h"
+
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  for (int i = 0; i < 32; ++i) {
+    if (!difftest_check_reg(reg_name(i), pc, ref_r->gpr[i], cpu.gpr[i])) {
+      return false;
+    }
+  }
+
+  if (!difftest_check_reg("pc", pc, ref_r->pc, cpu.pc)) {
+    return false;
+  }
+
+  return true;
+}
+
+void isa_difftest_attach() {
+}
