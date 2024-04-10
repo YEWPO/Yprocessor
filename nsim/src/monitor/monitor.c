5f46ef4117f5dcc0843e8e4909da222395443417
22050908-Meng Pei <2398696309@qq.com>
2024-04-10 15:23:52 +0800

    fix(monitor): npc is riscv64

--- nsim/src/monitor/monitor.c
+++ nsim/src/monitor/monitor.c
@@ -142,7 +142,7 @@ void init_monitor(int argc, char *argv[]) {
   IFDEF(CONFIG_ITRACE, init_disasm(
     MUXDEF(CONFIG_ISA_x86,     "i686",
     MUXDEF(CONFIG_ISA_mips32,  "mipsel",
-    MUXDEF(CONFIG_ISA_npc,      "npc",
+    MUXDEF(CONFIG_ISA_npc,      "riscv64",
     MUXDEF(CONFIG_ISA_riscv,
       MUXDEF(CONFIG_RV64,      "riscv64",
                                "riscv32"),
