230bc7dcca836109c74eb7db8ee20c58c7a2bbbb
22050908-Meng Pei <2398696309@qq.com>
2024-04-11 11:41:10 +0800

    feat: npc makefile

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,6 +12,8 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
+NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nsim-log.txt -t $(shell dirname $(IMAGE).elf)/nsim-trace.vcd -e "$(IMAGE).elf" #-b
+
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
@@ -19,3 +21,9 @@ image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run: image
+	$(MAKE) -C $(NSIM_HOME) ISA=$(ISA) run ARGS="$(NSIMFLAGS)" IMG=$(IMAGE).bin
+
+gdb: image
+	$(MAKE) -C $(NSIM_HOME) ISA=$(ISA) gdb ARGS="$(NSIMFLAGS)" IMG=$(IMAGE).bin
---
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -0,0 +1,8 @@
+include $(AM_HOME)/scripts/isa/riscv.mk
+include $(AM_HOME)/scripts/platform/npc.mk
+CFLAGS  += -DISA_H=\"riscv/riscv.h\"
+
+AM_SRCS += riscv/npc/start.S \
+           riscv/npc/cte.c \
+           riscv/npc/trap.S \
+           riscv/npc/vme.c
