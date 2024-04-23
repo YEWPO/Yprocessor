55c5099cc5e05589b8c236bc8de52b144c19c1ae
22050908-Meng Pei <2398696309@qq.com>
2024-04-23 15:51:09 +0800

    update(xbar): xbar stateReg rename to writeStateReg

--- npc/playground/src/memory/Xbar.scala
+++ npc/playground/src/memory/Xbar.scala
@@ -10,8 +10,11 @@ import chisel3.util.MuxCase
 import bus.Axi4WriteAddrBundle
 import bus.Axi4WriteDataBundle
 import bus.Axi4WriteRespBundle
+import bus.Axi4ReadDataBundle
+import bus.Axi4ReadAddrBundle
 
 object XbarReadState extends ChiselEnum {
+  val rIdle, rTimer, rSram = Value
 }
 
 object XbarWriteState extends ChiselEnum {
@@ -28,6 +31,11 @@ class Xbar extends Module {
       val b  = Flipped(Decoupled(new Axi4WriteRespBundle))
     }
 
+    val timer = new Bundle {
+      val ar = Decoupled(new Axi4ReadAddrBundle)
+      val r  = Flipped(Decoupled(new Axi4ReadDataBundle))
+    }
+
     val sram = new Axi4Bundle
   })
 
@@ -35,49 +43,49 @@ class Xbar extends Module {
   io.sram.r       <> io.axi.r
 
   import XbarWriteState._
-  val stateReg    = RegInit(wIdle)
-  val nextState   = WireDefault(wIdle)
-  stateReg        := nextState
+  val writeStateReg    = RegInit(wIdle)
+  val writeNextState   = WireDefault(wIdle)
+  writeStateReg := writeNextState
 
   val uartWrite   = io.axi.aw.valid && (io.axi.aw.bits.addr === UART_ADDR.U)
   val sramWrite   = io.axi.aw.valid && (io.axi.aw.bits.addr >= MEM_ADDR_BASE.U) && (io.axi.aw.bits.addr < MEM_ADDR_MAX.U)
 
-  io.uart.aw.valid := Mux(stateReg === wUart, io.axi.aw.valid, false.B)
+  io.uart.aw.valid := Mux(writeStateReg === wUart, io.axi.aw.valid, false.B)
   io.uart.aw.bits  := io.axi.aw.bits
-  io.uart.w.valid  := Mux(stateReg === wUart, io.axi.w.valid, false.B)
+  io.uart.w.valid  := Mux(writeStateReg === wUart, io.axi.w.valid, false.B)
   io.uart.w.bits   := io.axi.w.bits
-  io.uart.b.ready  := Mux(stateReg === wUart, io.axi.b.ready, false.B)
+  io.uart.b.ready  := Mux(writeStateReg === wUart, io.axi.b.ready, false.B)
 
-  io.sram.aw.valid := Mux(stateReg === wSram, io.axi.aw.valid, false.B)
+  io.sram.aw.valid := Mux(writeStateReg === wSram, io.axi.aw.valid, false.B)
   io.sram.aw.bits  := io.axi.aw.bits
-  io.sram.w.valid  := Mux(stateReg === wSram, io.axi.w.valid, false.B)
+  io.sram.w.valid  := Mux(writeStateReg === wSram, io.axi.w.valid, false.B)
   io.sram.w.bits   := io.axi.w.bits
-  io.sram.b.ready  := Mux(stateReg === wSram, io.axi.b.ready, false.B)
+  io.sram.b.ready  := Mux(writeStateReg === wSram, io.axi.b.ready, false.B)
 
   io.axi.aw.ready := MuxCase(false.B, Seq(
-    (stateReg === wUart) -> io.uart.aw.ready,
-    (stateReg === wSram) -> io.sram.aw.ready
+    (writeStateReg === wUart) -> io.uart.aw.ready,
+    (writeStateReg === wSram) -> io.sram.aw.ready
   ))
   io.axi.w.ready  := MuxCase(false.B, Seq(
-    (stateReg === wUart) -> io.uart.w.ready,
-    (stateReg === wSram) -> io.sram.w.ready
+    (writeStateReg === wUart) -> io.uart.w.ready,
+    (writeStateReg === wSram) -> io.sram.w.ready
   ))
   io.axi.b.valid  := MuxCase(false.B, Seq(
-    (stateReg === wUart) -> io.uart.b.valid,
-    (stateReg === wSram) -> io.sram.b.valid
+    (writeStateReg === wUart) -> io.uart.b.valid,
+    (writeStateReg === wSram) -> io.sram.b.valid
   ))
-  io.axi.b.bits   := Mux(stateReg === wUart, io.uart.b.bits, io.sram.b.bits)
+  io.axi.b.bits   := Mux(writeStateReg === wUart, io.uart.b.bits, io.sram.b.bits)
 
-  switch (stateReg) {
+  switch (writeStateReg) {
     is (wIdle) {
-      nextState := MuxCase(wIdle, Seq(
+      writeNextState := MuxCase(wIdle, Seq(
         uartWrite -> wUart,
         sramWrite -> wSram
       ))
     }
 
     is (wUart) {
-      nextState := MuxCase(wUart, Seq(
+      writeNextState := MuxCase(wUart, Seq(
         (io.uart.b.fire && uartWrite) -> wUart,
         (io.uart.b.fire && sramWrite) -> wSram,
         io.uart.b.fire -> wIdle
@@ -85,7 +93,7 @@ class Xbar extends Module {
     }
 
     is (wSram) {
-      nextState := MuxCase(wSram, Seq(
+      writeNextState := MuxCase(wSram, Seq(
         (io.sram.b.fire && uartWrite) -> wUart,
         (io.sram.b.fire && sramWrite) -> wSram,
         io.sram.b.fire -> wIdle
